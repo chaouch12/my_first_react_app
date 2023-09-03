212aa59d4cc5ece4346cea71273f1d107fc819e5
Walid Chaouch <walid.chaouch@check24.de>
2023-09-04 00:41:20 +0200

    0000: Add handleform(name, adress) to documents component

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useNavigate} from "react-router-dom";
 
 function Documents () {
 
-    const navigate = useNavigate()
+    //const [name, setName] = useState()
+    //const [adress, setAdress] = useState('')
+    const [condidate, setCondidate] = useState({})
+    const [data, setData] = useState([]) // array of condidates
+
+    const handleChange = (e) => {
+        const {name, value} =  e.target
+        setCondidate({...condidate, [name]:value} )
+        console.log('condidate saved is:', condidate)
+    }
+    const handleSubmit = (e) => {
+
+        e.preventDefault() // why?
+        setData([...data, condidate]);
+        setCondidate({name:'', adress:''})
+        //console.log('condidat is:', condidate)
+        console.log('data is:', data)
+    }
+
+    const checkValue = (e) => {
+        console.log(e.target)
+        console.log(e.target.value)
+    }
+
     return (
         <div>
             welcome in the Documents Component!
+            <br/>  <br/>
+            <form onSubmit={handleSubmit} className='form'>
+                Name:
+                <input
+                    placeholder="Enter your name"
+                    //onChange={ (e) => setName(e.target.value)}
+                    onChange={handleChange}
+                />
 
+                Adress:
+                <input
+                placeholder="Enter your Adress"
+                //onChange={ (e) => setAdress(e.target.value)}
+                onChange={handleChange}
+                />
+                <button type="submit">submit</button>
+            </form>
+
+
+            <div style={{marginTop: "20px"}}> This is my registred values</div>
+            <div>
+               todo: fetch data
+                {condidate.name},  {condidate.adress}
+            </div>
         </div>
 
     )
